cd29f2003ba846b36add91a0192aba89edb2c333
xxalfa <xxalfa@gmail.com>
2019-08-16 13:58:59 +0200

    Load admin list in scenario (#288)

--- 0.17/files/scenario.sh
+++ 0.17/files/scenario.sh
@@ -33,9 +33,10 @@ exec /opt/factorio/bin/x64/factorio \
   --port "$PORT" \
   --start-server-load-scenario "$SERVER_SCENARIO" \
   --server-settings "$CONFIG/server-settings.json" \
+  --server-banlist "$CONFIG/server-banlist.json" \
   --server-whitelist "$CONFIG/server-whitelist.json" \
   --use-server-whitelist \
-  --server-banlist "$CONFIG/server-banlist.json" \
+  --server-adminlist "$CONFIG/server-adminlist.json" \
   --rcon-port "$RCON_PORT" \
   --rcon-password "$(cat "$CONFIG/rconpw")" \
   --server-id /factorio/config/server-id.json
