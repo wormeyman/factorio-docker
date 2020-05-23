ef6bbde78dda6af39f755a07db98ea158c2d026e
Sandro <sandro.jaeckel@gmail.com>
2020-05-23 03:05:56 +0200

    Fix migration when config is mounted (#318)

--- 0.17/files/docker-entrypoint.sh
+++ 0.17/files/docker-entrypoint.sh
@@ -53,6 +53,8 @@ else
   SU_EXEC=""
 fi
 
+sed -i '/write-data=/c\write-data=\/factorio/' /opt/factorio/config/config.ini
+
 NRSAVES=$(find -L "$SAVES" -iname \*.zip -mindepth 1 | wc -l)
 if [[ $GENERATE_NEW_SAVE != true && $NRSAVES ==  0 ]]; then
     GENERATE_NEW_SAVE=true
